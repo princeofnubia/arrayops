430e53194c69fc5167afce975fa08363a078c9c6
Oyewo A O <abubakrilaitan@gmail.com>
2023-03-07 05:54:10 -0600

    more function

--- index.js
+++ index.js
@@ -105,3 +105,86 @@ class ArrayProc {
     return ind === undefined ? -1 : ind;
   }
 }
+
+function a(n) {
+  // base case
+  if (n == 0) {
+    return 1;
+  }
+  // recursive case
+  return n * a(n - 1);
+}
+
+function callMeAgain(n) {
+  // base case
+  if (n == 0) {
+    return;
+  }
+  // recursive case
+  console.log(n, ": call me again");
+  callMeAgain(n - 1);
+}
+
+callMeAgain(10);
+
+function flat(arr, depth) {
+  // base case;
+  let f = [];
+  let level = 1;
+  function flattener(arr, level) {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      if (Array.isArray(element)) {
+        if (depth == level) {
+          f.push(element);
+          break;
+        }
+        level++;
+        f.concat(flattener(element, level++));
+      } else {
+        f.push(element);
+      }
+    }
+    return f;
+  }
+
+  return flattener(arr, level);
+}
+
+function flat2(arr) {
+  // base case;
+  let f = [];
+  function flattener(arr) {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      if (Array.isArray(element)) {
+        f.concat(flattener(element));
+      } else {
+        f.push(element);
+      }
+    }
+    return f;
+  }
+
+  return flattener(arr);
+}
+
+function flatMap(arr, func) {
+  // base case;
+  let f = [];
+  function flattener(arr) {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      if (Array.isArray(element)) {
+        f.concat(flattener(element));
+      } else {
+        f.push(func(element));
+      }
+    }
+    return f;
+  }
+
+  return flattener(arr);
+}
+
+console.log(flatMap([1, [2, 3, 5]], (x) => x ** 2));
---
+++ recursion.js
@@ -0,0 +1,8 @@
+// a function that calls itself
+
+function a() {
+  console.log("call me again");
+  a();
+}
+
+a();
