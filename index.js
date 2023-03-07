a791a716d3d8f8ad8831c6d300cf503cbf023da3
Oyewo A O <abubakrilaitan@gmail.com>
2023-03-07 03:50:21 -0600

    more function

--- index.js
+++ index.js
@@ -43,11 +43,65 @@ class ArrayProc {
     }
     return true;
   }
+
+  fill(value, start, end = this.arr.length) {
+    const arra = this.arr;
+    for (let index = start; index < end; index++) {
+      arra[index] = value;
+    }
+    return arra;
+  }
+
+  filter(func) {
+    let filtered = [];
+    for (let index = 0; index < this.arr.length; index++) {
+      if (func(this.arr[index])) {
+        filtered.push(this.arr[index]);
+      }
+    }
+    return filtered;
+  }
+
+  find(func) {
+    let filtered = [];
+    for (let index = 0; index < this.arr.length; index++) {
+      if (func(this.arr[index])) {
+        filtered.push(this.arr[index]);
+        break;
+      }
+    }
+    return filtered.length === 0 ? undefined : filtered;
+  }
+
+  findIndex(func) {
+    let ind;
+    for (let index = 0; index < this.arr.length; index++) {
+      if (func(this.arr[index])) {
+        ind = index;
+        break;
+      }
+    }
+    return ind === undefined ? -1 : ind;
+  }
+
+  findIndexR(func) {
+    let ind;
+    for (let index = this.arr.length - 1; index >= 0; index--) {
+      if (func(this.arr[index])) {
+        ind = index;
+        break;
+      }
+    }
+    return ind === undefined ? -1 : ind;
+  }
+
+  findIndexB(func) {
+    let ind;
+    for (let index = 0; index < this.arr.length; index++) {
+      if (func(this.arr[index])) {
+        ind = index;
+      }
+    }
+    return ind === undefined ? -1 : ind;
+  }
 }
-
-let d = new ArrayProc([7, 6, 9, 6, 8, 10, 44, 23]);
-console.log(d.concat([9, 4, 5]));
-
-const greaterThan5 = (num) => num > 5;
-
-console.log(d.every(greaterThan5));
